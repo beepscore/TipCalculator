d30fc9c832c9c9e5e140767e08b31c137eb6975a
beepscore <support@beepscore.com>
2016-01-02 22:50:08 -0800

    In TipCalculatorTests delete templated test methods and add testReturnPossibleTips

--- TipCalculatorTests/TipCalculatorTests.swift
+++ TipCalculatorTests/TipCalculatorTests.swift
@@ -11,26 +11,20 @@ import XCTest
 
 class TipCalculatorTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
+    func testReturnPossibleTips() {
+        let testTotal = 29.37
+        let tipCalculator = TipCalculatorModel(total: testTotal, taxPct: 15)
+        // [18: (0.3304125, 29.7004125), 15: (0.27534375, 29.64534375), 20: (0.367125, 29.737125)]
+        let actual = tipCalculator.returnPossibleTips()
+        print("actual: ", actual)
+        XCTAssertEqualWithAccuracy(29.70, actual[18]!.total, accuracy: 0.001)
+        XCTAssertEqualWithAccuracy(0.330, actual[18]!.tipAmt, accuracy: 0.001)
+
+        XCTAssertEqualWithAccuracy(29.645, actual[15]!.total, accuracy: 0.001)
+        XCTAssertEqualWithAccuracy(0.2753, actual[15]!.tipAmt, accuracy: 0.001)
+
+        XCTAssertEqualWithAccuracy(29.737, actual[20]!.total, accuracy: 0.001)
+        XCTAssertEqualWithAccuracy(0.367, actual[20]!.tipAmt, accuracy: 0.001)
     }
     
 }
