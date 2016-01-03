57682197101c61338279cc32840409da69eefc47
beepscore <support@beepscore.com>
2016-01-02 23:20:13 -0800

    In TipCalculatorTests testReturnPossibleTips fix argument taxPct. Add testCalcTipWithTipPct.

--- TipCalculatorTests/TipCalculatorTests.swift
+++ TipCalculatorTests/TipCalculatorTests.swift
@@ -10,21 +10,32 @@ import UIKit
 import XCTest
 
 class TipCalculatorTests: XCTestCase {
-    
+
+    func testCalcTipWithTipPct() {
+        let testTotal = 29.37
+        let tipCalculator = TipCalculatorModel(total: testTotal, taxPct: 0.15)
+
+        let actual = tipCalculator.calcTipWithTipPct(0.12)
+        XCTAssertEqualWithAccuracy(3.065, actual.tipAmt, accuracy: 0.001)
+        XCTAssertEqualWithAccuracy(32.4347, actual.total, accuracy: 0.001)
+    }
+
     func testReturnPossibleTips() {
         let testTotal = 29.37
-        let tipCalculator = TipCalculatorModel(total: testTotal, taxPct: 15)
-        // [18: (0.3304125, 29.7004125), 15: (0.27534375, 29.64534375), 20: (0.367125, 29.737125)]
+        let tipCalculator = TipCalculatorModel(total: testTotal, taxPct: 0.15)
+
         let actual = tipCalculator.returnPossibleTips()
-        print("actual: ", actual)
-        XCTAssertEqualWithAccuracy(29.70, actual[18]!.total, accuracy: 0.001)
-        XCTAssertEqualWithAccuracy(0.330, actual[18]!.tipAmt, accuracy: 0.001)
+        // [18: (4.59704347826087, 33.9670434782609),
+        //  15: (3.83086956521739, 33.2008695652174),
+        //  20: (5.10782608695652, 34.4778260869565)]
+        XCTAssertEqualWithAccuracy(4.597, actual[18]!.tipAmt, accuracy: 0.001)
+        XCTAssertEqualWithAccuracy(33.967, actual[18]!.total, accuracy: 0.001)
 
-        XCTAssertEqualWithAccuracy(29.645, actual[15]!.total, accuracy: 0.001)
-        XCTAssertEqualWithAccuracy(0.2753, actual[15]!.tipAmt, accuracy: 0.001)
+        XCTAssertEqualWithAccuracy(3.8309, actual[15]!.tipAmt, accuracy: 0.001)
+        XCTAssertEqualWithAccuracy(33.201, actual[15]!.total, accuracy: 0.001)
 
-        XCTAssertEqualWithAccuracy(29.737, actual[20]!.total, accuracy: 0.001)
-        XCTAssertEqualWithAccuracy(0.367, actual[20]!.tipAmt, accuracy: 0.001)
+        XCTAssertEqualWithAccuracy(5.1078, actual[20]!.tipAmt, accuracy: 0.001)
+        XCTAssertEqualWithAccuracy(34.4778, actual[20]!.total, accuracy: 0.001)
     }
     
 }
